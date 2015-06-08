9baa2b504c7570a51dfa65afa29e622912a5fbe2
David Durieux <d.durieux@siprossii.com>
2015-06-08 14:42:53 +0200

    Begin update toolbar / menu and list of assets in search

--- app.js
+++ app.js
@@ -111,6 +111,30 @@ app.controller('QueryBuilderCtrl', ['$scope', 'Restangular', '$http', '$rootScop
   }]);
 
 
+    app.controller('AppMenu', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildToggler(navID) {
+          var debounceFn =  $mdUtil.debounce(function(){
+                $mdSidenav(navID)
+                  .toggle()
+                  .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                  });
+              },300);
+
+          return debounceFn;
+        }
+
+    });
+
+
 
 function homePage($scope, Restangular) {
     
